c9fdf5271aacf210ed5b68498577970ee8de14b5
Gabriel Moraes <gabrielhm2001@gmail.com>
2021-11-14 13:28:35 -0300

    fix: fix font face

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import ReactDOM from 'react-dom'
 
+import '@/styles/fonts.css'
 import { App } from '@/App'
 
 ReactDOM.render(
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,38 +1,6 @@
 import { createGlobalStyle, css } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
-  @font-face {
-    font-family: 'Raleway';
-    font-style: normal;
-    font-weight: 700;
-    src: local(''),
-        url('../fonts/raleway-v22-latin-regular.woff2') format('woff2'),
-  }
-
-  @font-face {
-    font-family: 'Montserrat';
-    font-style: normal;
-    font-weight: 400;
-    src: local(''),
-        url('/fonts/montserrat-v18-latin-regular.woff2') format('woff2'),
-  }
-
-  @font-face {
-    font-family: 'Montserrat';
-    font-style: normal;
-    font-weight: 500;
-    src: local(''),
-        url('/fonts/montserrat-v18-latin-500.woff2') format('woff2'),
-  }
-
-  @font-face {
-    font-family: 'Montserrat';
-    font-style: normal;
-    font-weight: 600;
-    src: local(''),
-        url('/fonts/montserrat-v18-latin-600.woff2') format('woff2'),
-  }
-
   * {
     margin: 0;
     padding: 0;
